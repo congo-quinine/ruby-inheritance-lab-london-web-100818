8ca6533891733c1690ccfd300cb62a187705f67c
congo-quinine <congo.quinine@gmail.com>
2018-10-16 09:39:52 +0100

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,19 @@
-class Student 
+class Student < User
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(knowledge)
+    @knowledge << knowledge
+  end
+
+  def knowledge
+    @knowledge
+  end
 
 end
+
+
+# 6. Define a method, `#learn`, that takes in a string and adds that string to the student's `@knowledge` array.
+# 7. Define a method, `#knowledge`, that returns that student's knowledge array.
